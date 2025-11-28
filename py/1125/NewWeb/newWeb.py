48dd79f0a0ddb64393f03d3350cd8084adad3b22
Eric J. Park <erik.j.park@gmail.com>
2025-11-28 18:08:20 +0900

    eod

--- py/1125/NewWeb/newWeb.py
+++ py/1125/NewWeb/newWeb.py
@@ -6,14 +6,23 @@ app = FastAPI()
 def xmlTest():
     xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
     xml += "<snacks>"
-    xml +="<snack>"
-    xml +="<name>초코파이</name>"
-    xml +="<price>5000</price>"
-    xml +="<name>새우깡</name>"
-    xml +="<price>3000</price>"
-    xml +="</snack>"
-    xml +="</snacks>"
-    return Response(xml, media_type="application/xml")
+    
+    # 첫 번째 상품 (하나의 유효한 <snack> 요소)
+    xml += "<snack>"
+    xml +="<s_name>초코파이</s_name>" # 클라이언트 스크립트에 맞게 태그 이름 변경
+    xml +="<s_price>5000</s_price>" # 클라이언트 스크립트에 맞게 태그 이름 변경
+    xml += "</snack>"
+    
+    # 두 번째 상품 (두 번째 유효한 <snack> 요소)
+    xml += "<snack>"
+    xml +="<s_name>새우깡</s_name>" # 클라이언트 스크립트에 맞게 태그 이름 변경
+    xml +="<s_price>3000</s_price>" # 클라이언트 스크립트에 맞게 태그 이름 변경
+    xml += "</snack>"
+    
+    xml += "</snacks>"
+    
+    h = {"Access-Control-Allow-Origin" : "*"}
+    return Response(xml, media_type="application/xml", headers=h)
 
 @app.get("/json.test")
 def jsonTest():
