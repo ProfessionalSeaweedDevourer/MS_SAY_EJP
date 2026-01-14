3f5720028a13f1e41bba2f3c0f0566d55854ad91
Eric J. Park <erik.j.park@gmail.com>
2026-01-14 09:37:42 +0900

    Catchup Jan14

---
+++ C03_Newweb/jan13/testproject001/src/Test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>
+        <button onClick={alert("버튼 눌림")}>
+            알림 출력 버튼
+        </button>
+    </div>
+  )
+}
+
+export default Test
